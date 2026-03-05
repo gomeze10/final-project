2fa9a9781de185a312690cccff84a01eb28a0709
gomeze10 <gomezellianna2010@gmail.com>
2026-03-05 12:04:47 -0500

    dont look at this yet, its rlly bad

---
+++ quiz.py
@@ -0,0 +1,11 @@
+class Quiz:
+    def __init__(self):
+        self.answers = []
+
+    def add_answer(self, answer):
+        self.answers.append(answer)
+
+dog_ages[]
+
+print("which one of these best describes your dog's age?")
+print("Options: puppy, adolescent, adult")
